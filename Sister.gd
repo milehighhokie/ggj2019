fdf32d6f67fc645f5414fec89e0bea400823f681
LRFLEW <LRFLEW@aol.com>
2019-01-27 13:46:12 -0700

    Add Sister Hug Achievable

---
+++ Sister.gd
@@ -0,0 +1,30 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var _overlap = false
+var _triggered = false
+
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    pass
+
+func _process(delta):
+    # Called every frame. Delta is time since last frame.
+    # Update game logic here.
+    if !_triggered && _overlap && Input.is_action_pressed('hug'):
+        get_node("../Achievement/AnimationPlayer").play("Popup")
+        _triggered = true
+    pass
+
+func _on_Area2D_body_entered(body):
+    _overlap = true
+    pass # replace with function body
+
+
+func _on_Area2D_body_exited(body):
+    _overlap = false
+    pass # replace with function body
